f219e3577932454d0c5f6beb711401603666d491
postfm <postfm@yandex.ru>
2023-06-03 16:10:25 +0300

    Реализовал замечания наставника

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -75,8 +75,8 @@ class PointsModel extends Observable {
     const adaptedPoint = {
       ...point,
       basePrice: point['base_price'],
-      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
-      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      dateFrom: point['date_from'] ?? new Date(point['date_from']),
+      dateTo: point['date_to'] ?? new Date(point['date_to']),
       isFavorite: point['is_favorite'],
     };
 
